04e333b227fd30012068ea8f40c0ab2f73b73786
Tuomas Räsänen <tuos@codegrove.org>
2010-04-24 00:36:37 +0300

    Renamed libnotetaker to libm210 because that's what it really is: API for M210

---
+++ libm210/m210.h
@@ -0,0 +1,137 @@
+#ifndef M210_H
+#define M210_H
+
+#define M210_MODE_XY     0x01
+#define M210_MODE_TABLET 0x02
+
+#define M210_ORIENTATION_TOP   0x00
+#define M210_ORIENTATION_LEFT  0x01
+#define M210_ORIENTATION_RIGHT 0x02
+
+#define M210_SCALE_0   0x00
+#define M210_SCALE_1   0x01
+#define M210_SCALE_2   0x02
+#define M210_SCALE_3   0x03
+#define M210_SCALE_4   0x04
+#define M210_SCALE_5   0x05
+#define M210_SCALE_6   0x06
+#define M210_SCALE_7   0x07
+#define M210_SCALE_8   0x08
+#define M210_SCALE_9   0x09
+#define M210_SCALE_MIN M210_SCALE_0
+#define M210_SCALE_MAX M210_SCALE_9
+
+#define M210_IFACE_COUNT 2
+
+struct m210_info {
+    uint16_t firmware_version;
+    uint16_t analog_version;
+    uint16_t pad_version;
+    uint8_t mode;
+};
+
+enum m210_err {
+    err_ok,
+    err_sys,
+    err_baddev,
+    err_nodev,
+    err_badmsg
+};
+
+/**
+   An opaque object representing a M210 device.
+*/
+typedef struct m210* m210_t;
+
+typedef enum m210_err m210_err_t;
+
+/**
+   Open a hid connetion to a M210 device and return an object
+   representing it.
+
+   Succesfully opened device must be freed with m210_free().
+
+   Return values:
+
+   - err_ok - success
+
+   - err_sys - system call failed and errno is set appropriately
+
+   - err_nodev - a M210 device was not found
+
+   - err_baddev - hidraw_paths did not represent interfaces 0 and 1 of
+   a M210 device.
+
+   @m210 an address of an object representing the M210 device.
+
+   @hidraw_paths a list of two null-terminated paths of hidraw device
+   nodes for interface 0 and 1 respectively or NULL in which case
+   proper nodes are searched with udev.
+
+*/
+m210_err_t m210_open(m210_t *m210, char** hidraw_paths);
+
+/**
+   Close the M210 device connection and free all resources.
+
+   Return values:
+
+   - err_ok - success
+
+   - err_sys - system call failed and errno is set
+   appropriately. Memory is not freed!
+
+   @m210 an object represeting the M210 device.
+
+*/
+m210_err_t m210_free(m210_t m210);
+
+/**
+   Return information about the M210 device.
+
+   Return values:
+
+   - err_ok - success
+
+   - err_sys - system call failed and errno is set appropriately
+
+   @m210 an object represeting the M210 device.
+
+   @info an address where the info will be stored.
+
+*/
+m210_err_t m210_get_info(m210_t m210, struct m210_info *info);
+
+/**
+   Requests the M210 device to delete all notes.
+
+   Return values:
+
+   - err_ok - success
+
+   - err_sys - system call failed and errno is set appropriately
+
+   @m210 an object represeting the M210 device.
+
+*/
+m210_err_t m210_delete_notes(m210_t m210);
+
+/**
+   Return size of stored notes.
+
+   Return values:
+
+   - err_ok - success
+
+   - err_sys - system call failed and errno is set appropriately
+
+   - err_badmsg - device sent unexpected message
+
+   @m210 an object represeting the M210 device.
+
+   @size an address where the size will be stored.
+
+*/
+m210_err_t m210_get_data_size(m210_t m210, size_t *size);
+
+#endif /* M210_H */
