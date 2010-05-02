00ebabac56cdaa3c4189c9b330a9458bfc6c1f87
Tuomas Räsänen <tuos@codegrove.org>
2010-05-02 19:02:11 +0300

    Include stdio.h in header instead of source file.
      * FILE* is used in function signatures.

--- src/m210.c
+++ src/m210.c
@@ -15,7 +15,6 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
