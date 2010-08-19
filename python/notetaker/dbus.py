b9c7ae2175aac1831ce745d01ccccfbbcfa56676
Tuomas Räsänen (tuos) <tuos@codegrove.org>
2010-08-19 13:17:16 +0300

    python: minimal daemon working

--- python/notetaker/dbus.py
+++ python/notetaker/dbus.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 
+import os.path
+import syslog
+
 # Third-party modules.
 import dbus
 import dbus.service
@@ -7,19 +10,39 @@ import dbus.mainloop.glib
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
+import linux.input
+import pegatech
+
 SERVICE_NAME = 'org.codegrove.notetaker'
 DAEMON_INTERFACE_NAME = 'org.codegrove.notetaker.daemon'
 
+class M210Connection(object):
+
+    def __init__(self, hidraw0_path, hidraw1_path, event_path):
+        self._event_fd = None
+        self._event_fd = os.open(event_path, os.O_RDWR)
+        linux.input.grab_event_device(self._event_fd)
+        self.m210 = pegatech.M210((hidraw0_path, hidraw1_path))
+
+    def __del__(self):
+        if self._event_fd is not None:
+            linux.input.release_event_device(self._event_fd)
+
 class Daemon(dbus.service.Object):
 
-    OBJECT_PATH = '/org/codegrove/notetaer/daemon'
+    OBJECT_PATH = '/org/codegrove/notetaker/daemon'
 
     def __init__(self, bus):
         dbus.service.Object.__init__(self, bus, Daemon.OBJECT_PATH)
+        self._m210_connections = []
+        self._triplet = [None, None, None]
 
-    @dbus.service.method(DAEMON_INTERFACE_NAME, in_signature='s', out_signature='s')
-    def hello(self, name):
-        return 'Hola %s!' % name
+    @dbus.service.method(DAEMON_INTERFACE_NAME, in_signature='iss', out_signature='')
+    def m210_connected(self, index, udev_root, kernel_name):
+        self._triplet[index] = os.path.join(udev_root, kernel_name)
+        if not None in self._triplet:
+            self._m210_connections.append(M210Connection(*self._triplet))
+            self._triplet = [None, None, None]
 
 def attach():
     global _name
