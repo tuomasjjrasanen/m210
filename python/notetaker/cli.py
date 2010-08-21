f7653c702af68347228457ac223c47952402add6
Tuomas Räsänen (tuos) <tuos@codegrove.org>
2010-08-21 21:14:19 +0300

    python: extracted a bit more common cli code into notetaker.cli

--- python/notetaker/cli.py
+++ python/notetaker/cli.py
@@ -14,3 +14,12 @@ class OptionParser(optparse.OptionParser):
         kwargs['version'] = VERSION_TEXT
         optparse.OptionParser.__init__(self, *args, **kwargs)
 
+def _print_devices(dirpaths, status):
+    for dirpath in dirpaths:
+        print status, dirpath
+
+def print_disconnected_devices(dirpaths):
+    return _print_devices("D", dirpaths)
+
+def print_connected_devices(dirpaths):
+    return _print_devices("C", dirpaths)
