76df6d220a24ec03fc0dfc751435d21ffc5d9247
Tuomas Räsänen (tuos) <tuos@codegrove.org>
2010-08-22 22:20:29 +0300

    noter: core => optparse because that's what it really was

--- noter/noter/setup.py
+++ noter/noter/setup.py
@@ -2,10 +2,10 @@
 
 from distutils.core import setup
 
-setup(name='noter-core',
+setup(name='noter',
       version='0.1',
       author='Tuomas Räsänen',
       author_email='tuos@codegrove.org',
       package_dir={'noter': 'src/lib'},
-      packages=['noter', 'noter.core'],
+      packages=['noter'],
       )
