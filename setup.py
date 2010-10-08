997ca0f19c0a80e6e0f26cd29d5eeef8f2da706e
Tuomas Räsänen (tuos) <tuos@codegrove.org>
2010-10-09 00:02:39 +0300

    New structure and names for components.

--- setup.py
+++ setup.py
@@ -26,36 +26,36 @@ class build_py(_build_py):
 
     def run(self):
         try:
-            filenames = os.listdir('qt/ui')
+            filenames = os.listdir('m210c-qt/ui')
         except OSError, e:
             if e.errno == errno.ENOENT:
                 # ui directory does not exists. fine, no ui-files then.
                 filenames = []
             raise e # Something else went wrong during listing, panic!
 
-        # qt/ui/*.ui -> qt/src/lib/ui/*.py
-        for filename in os.listdir('qt/ui'):
+        # m210c-qt/ui/*.ui -> m210c-qt/src/lib/ui/*.py
+        for filename in os.listdir('m210c-qt/ui'):
             filename = os.path.basename(filename)
             name, ext = os.path.splitext(filename)
             if ext != '.ui':
                 continue
-            src_path = 'qt/ui/%s' % filename
+            src_path = 'm210c-qt/ui/%s' % filename
             source_mtime = os.stat(src_path).st_mtime
-            dest_path = 'qt/src/lib/ui/%s.py' % name
+            dest_path = 'm210c-qt/src/lib/ui/%s.py' % name
             if not os.path.exists(dest_path):
                 dest_mtime = -1
             else:
                 dest_mtime = os.stat(dest_path).st_mtime
             if source_mtime > dest_mtime:
-                os.system('pyuic4 -o qt/src/lib/ui/%s.py qt/ui/%s' % (name, filename))
+                os.system('pyuic4 -o m210c-qt/src/lib/ui/%s.py m210c-qt/ui/%s' % (name, filename))
 
         return _build_py.run(self) # Proceed normally.
 
 modulehidraw = Extension('m210.hidraw',
-                         sources=['pylib/src/lib/modulehidraw.c'])
+                         sources=['python-m210/src/lib/modulehidraw.c'])
 
 moduleinput = Extension('m210.input',
-                         sources=['pylib/src/lib/moduleinput.c'])
+                         sources=['python-m210/src/lib/moduleinput.c'])
 
 setup(name='m210',
       provides=[
@@ -106,42 +106,42 @@ setup(name='m210',
         "Topic :: Education",
         ],
       package_dir={ # One for each module.
-        'm210':        'pylib/src/lib',
-        'm210.daemon': 'daemon/src/lib',
-        'm210.cli':    'cli/src/lib',
-        'm210.qt':     'qt/src/lib',
+        'm210':           'python-m210/src/lib',
+        'm210.daemon':    'm210d/src/lib',
+        'm210.client':    'm210c/src/lib',
+        'm210.client.qt': 'm210c-qt/src/lib',
         },
       packages=[
         'm210',
-        'm210.cli',
+        'm210.client',
         'm210.daemon',
-        'm210.qt',
-        'm210.qt.ui',
+        'm210.client.qt',
+        'm210.client.qt.ui',
         ],
       scripts=[
-        'daemon/src/bin/m210-daemon',
-        'cli/src/bin/m210-connect',
-        'cli/src/bin/m210-disconnect',
-        'cli/src/bin/m210-download',
-        'cli/src/bin/m210-info',
-        'cli/src/bin/m210-erase',
-        'cli/src/bin/m210-export-notes-as-svgs',
-        'qt/src/bin/m210-qt',
+        'm210d/src/bin/m210d',
+        'm210c/src/bin/m210c-connect',
+        'm210c/src/bin/m210c-disconnect',
+        'm210c/src/bin/m210c-info',
+        'm210c/src/bin/m210c-erase',
+        'm210c/src/bin/m210c-download',
+        'm210c/src/bin/m210c-export-notes-to-svgs',
+        'm210c-qt/src/bin/m210c-qt',
         ],
       data_files=[
         ('/etc/dbus-1/system.d',
          [
-                'daemon/etc/dbus-1/system.d/org.codegrove.m210.daemon.conf',
+                'm210d/etc/dbus-1/system.d/org.codegrove.m210.daemon.conf',
                 ],
          ),
         ('/etc/init',
          [
-                'daemon/etc/init/m210-daemon.conf',
+                'm210d/etc/init/m210d.conf',
                 ],
          ),
         ('/etc/udev/rules.d',
          [
-                'daemon/etc/udev/rules.d/50-m210-daemon.rules',
+                'm210d/etc/udev/rules.d/50-m210d.rules',
                 ],
          ),
         ],
