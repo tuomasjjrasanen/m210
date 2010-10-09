9027177b67d9cbd95a31f4fa977caf5c57b970a6
Tuomas Räsänen (tuos) <tuos@codegrove.org>
2010-10-09 15:35:58 +0300

    python-m210: got rid of collections-module.

--- python-m210/src/pegatech.py
+++ python-m210/src/pegatech.py
@@ -28,13 +28,79 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+import collections
 import errno
 import os
 import select
 import struct
 
 import m210.hidraw
-import m210.collections
+
+class OrderedSet(collections.MutableSet):
+
+    KEY, PREV, NEXT = range(3)
+
+    def __init__(self, iterable=None):
+        self.end = end = [] 
+        end += [None, end, end]         # sentinel node for doubly linked list
+        self.map = {}                   # key --> [key, prev, next]
+        if iterable is not None:
+            self |= iterable
+
+    def __getitem__(self, i):
+        return tuple(self)[i]
+
+    def __len__(self):
+        return len(self.map)
+
+    def __contains__(self, key):
+        return key in self.map
+
+    def add(self, key):
+        if key not in self.map:
+            end = self.end
+            curr = end[OrderedSet.PREV]
+            curr[OrderedSet.NEXT] = end[OrderedSet.PREV] = self.map[key] = [key, curr, end]
+
+    def discard(self, key):
+        if key in self.map:        
+            key, prev, next = self.map.pop(key)
+            prev[OrderedSet.NEXT] = next
+            next[OrderedSet.PREV] = prev
+
+    def __iter__(self):
+        end = self.end
+        curr = end[OrderedSet.NEXT]
+        while curr is not end:
+            yield curr[OrderedSet.KEY]
+            curr = curr[OrderedSet.NEXT]
+
+    def __reversed__(self):
+        end = self.end
+        curr = end[OrderedSet.PREV]
+        while curr is not end:
+            yield curr[OrderedSet.KEY]
+            curr = curr[OrderedSet.PREV]
+
+    def pop(self, last=True):
+        if not self:
+            raise KeyError('set is empty')
+        key = next(reversed(self)) if last else next(iter(self))
+        self.discard(key)
+        return key
+
+    def __repr__(self):
+        if not self:
+            return '%s()' % (self.__class__.__name__,)
+        return '%s(%r)' % (self.__class__.__name__, list(self))
+
+    def __eq__(self, other):
+        if isinstance(other, OrderedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return set(self) == set(other)
+
+    def __del__(self):
+        self.clear()                    # remove circular references
 
 __all__ =  [
     "CommunicationError",
