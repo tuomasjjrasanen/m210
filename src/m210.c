1d87f237dbd236333b4d3deadd8e887eb4b4f26c
Tuomas Jorma Juhani Räsänen <tuomasjjrasanen@tjjr.fi>
2012-01-08 16:16:33 +0200

    Replaced © with (C) for possible robustness?

--- src/m210.c
+++ src/m210.c
@@ -1,5 +1,5 @@
 /* m210 - Pegasus Tablet Mobile NoteTaker (M210) Controller
- * Copyright © 2011 Tuomas Jorma Juhani Räsänen
+ * Copyright (C) 2011 Tuomas Jorma Juhani Räsänen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -42,7 +42,7 @@ static void print_help_hint(void)
 static void print_version(void)
 {
 	printf("%s %s\n"
-	       "Copyright © 2011 Tuomas Jorma Juhani Räsänen\n"
+	       "Copyright (C) 2011 Tuomas Jorma Juhani Räsänen\n"
 	       "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
 	       "This is free software: you are free to change and redistribute it.\n"
 	       "There is NO WARRANTY, to the extent permitted by law.\n"
